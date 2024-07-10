486521c4d2c6c8f7324ad5052c7d88288fec71a4
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2024-07-10 15:37:54 +0530

    Create l2_switch_case.java

---
+++ l2_switch_case.java
@@ -0,0 +1,30 @@
+package day2;
+import java.util.Scanner; 
+
+public class Variables {
+	public static void main(String[] args) {
+	
+		 int v=1;
+		 switch(v) {
+		 case 1:System.out.println("Monday");break;
+		 case 2:System.out.println("Tuesday");break;
+		 case 3:System.out.println("Wednesday");break;
+		 case 4:System.out.println("Thursday");break;
+		 case 5:System.out.println("Friday");break;
+		 case 6:System.out.println("Saturday");break;
+		 case 7:System.out.println("Sunday");break;
+		 default:System.out.println("Invalid Week number");
+		 }
+          
+        
+        
+
+
+		
+		
+		
+		
+		
+	}
+
+}
