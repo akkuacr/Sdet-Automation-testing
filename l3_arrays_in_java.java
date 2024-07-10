37d7be73773668265c52194e01893f893029aa0b
Akash Chand <111640882+akkuacr@users.noreply.github.com>
2024-07-10 16:55:16 +0530

    Create l3_arrays_in_java.java

---
+++ l3_arrays_in_java.java
@@ -0,0 +1,103 @@
+package day2;
+import java.util.Scanner; 
+
+public class Variables {
+	public static void main(String[] args) {
+//	
+////		 Scanner sc = new Scanner(System.in);
+////		// System.out.println("enter the number");
+////		 int a[] = new int[5];
+////		 //here also indexing starts from 0
+////		 //declaring array
+////		 
+////		 
+////		 a[0]=100;
+////		 a[1]=200;
+////		 a[2]=300;
+////		 a[3]=400;
+////		 a[4]=500;
+////		 
+////		 //approach2 
+////		 int a1[]= {1000,20000,300,900,1000};
+////		 
+////		 
+////		 //find length of an array
+////		 System.out.println("length of the array is"+a.length);
+////		 
+////		 //read single value of an array
+////		 System.out.println(a[4]);
+////		 
+////		 //printing all values of an array
+////		 
+////		 for(int i=0;i<a.length;i++) {
+////			 System.out.println(a[i]);
+////			 }
+////		 
+////		 
+////		 //enhanced for loop
+////		 //for each loop 
+////		 for(int x:a)
+////		 {
+////			 System.out.println(x);
+////		 }
+////		 
+//		 
+//		
+//		int a[][] = new int[3][2];
+//		a[0][0]=100;
+//		a[0][1]=200;
+//		a[1][0]=300;
+//		a[1][1]=400;
+//		a[2][0]=500;
+//		
+////	int a[][]= new int [19][10];
+////	int []a[]= new int[19][10];
+////	int [][]a = new int [10][20];
+////	all three methods are valid same goes with the single array
+////
+//		//approach2 
+//		int a2[][] = {{100,200},{500,600}};
+//		
+//		
+//		//printing size of array
+//		System.out.println("length of rows"+a.length);
+//		System.out.println("length of rows"+a[0].length);
+//		
+//		
+//		//read single value from an array
+//		
+//		System.out.println(a[2][1]);
+//		
+//		
+////		aur baki retrieving ka tarika same as c++ hi h
+//		
+//	//enhanced for loop 
+//		for(int arr[]:a)
+//		{
+//			for(int x:arr) {
+//				System.out.println(x+" ");
+//			}
+//		}
+//		 
+//		
+////		to create heterogenous array means you  have to declare object data
+//	
+		
+//		bss yeh chij nayi h
+		
+		
+		Object a[]= {100,10.5,'A',"Welcome",true};
+		
+		for(Object x:a)
+	   { 
+			System.out.println(x);
+		
+	   }
+		 
+		 
+		 
+		 
+          	
+	}
+
+}
